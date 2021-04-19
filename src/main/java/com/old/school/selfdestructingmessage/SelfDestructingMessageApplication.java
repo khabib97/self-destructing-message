23784086c833678c024934a8457f91e3841fe027
Kawser Habib <kawser.habib.dev@gmail.com>
2021-04-19 23:07:30 +0600

    init repo

---
+++ src/main/java/com/old/school/selfdestructingmessage/SelfDestructingMessageApplication.java
@@ -0,0 +1,13 @@
+package com.old.school.selfdestructingmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfDestructingMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfDestructingMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/old/school/selfdestructingmessage/SelfDestructingMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.old.school.selfdestructingmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfDestructingMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
