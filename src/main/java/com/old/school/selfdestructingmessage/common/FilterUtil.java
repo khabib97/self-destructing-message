fc1af06a30bae23364c4845c48af649100cb4fed
Kawser Habib <kawser.habib.dev@gmail.com>
2021-04-20 21:35:59 +0600

    return data filter created

---
+++ src/main/java/com/old/school/selfdestructingmessage/common/FilterUtil.java
@@ -0,0 +1,28 @@
+package com.old.school.selfdestructingmessage.common;
+
+import java.util.Set;
+
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
+@Component
+public class FilterUtil {
+
+	public <E> MappingJacksonValue dynamicFilter(E fiterData, Class<?> source  , Set<String> fieldsNotFiltered) {
+		
+		JsonFilter jsonFilter = (JsonFilter) source.getAnnotation(JsonFilter.class);
+ 		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept(fieldsNotFiltered);
+		FilterProvider filters = new SimpleFilterProvider().addFilter(jsonFilter.value(), filter);
+
+		MappingJacksonValue mapping = new MappingJacksonValue(fiterData);
+		mapping.setFilters(filters);
+		
+		return mapping;
+	}
+
+}
