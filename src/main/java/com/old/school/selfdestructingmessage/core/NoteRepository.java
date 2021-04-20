e98683f89a8ca26499a39af6e2aa6b05067e636d
Kawser Habib <kawser.habib.dev@gmail.com>
2021-04-20 21:38:08 +0600

    implement custom custom query

--- src/main/java/com/old/school/selfdestructingmessage/core/NoteRepository.java
+++ src/main/java/com/old/school/selfdestructingmessage/core/NoteRepository.java
@@ -1,11 +1,17 @@
 package com.old.school.selfdestructingmessage.core;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NoteRepository extends JpaRepository<Note, String> {
 
 	List<Note> findAll();
-
+	
+	@Query("SELECT u FROM Note u WHERE u.id = :id and u.isActive = :isActive")
+	Optional<Note> findByIdIfActive(@Param("id")String id, @Param("isActive") boolean isActive);
+	
 }
