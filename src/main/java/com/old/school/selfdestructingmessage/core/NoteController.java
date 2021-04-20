284930ad4fad5b9883aa9e33798e04453b2fbaec
Kawser Habib <kawser.habib.dev@gmail.com>
2021-04-20 22:46:26 +0600

    filtering json response

--- src/main/java/com/old/school/selfdestructingmessage/core/NoteController.java
+++ src/main/java/com/old/school/selfdestructingmessage/core/NoteController.java
@@ -2,16 +2,21 @@ package com.old.school.selfdestructingmessage.core;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Set;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.old.school.selfdestructingmessage.common.FilterUtil;
 
 
 @RestController
@@ -19,14 +24,25 @@ public class NoteController {
 	
 	@Autowired
 	private NoteService noteService;
+	@Autowired
+	private FilterUtil filterUtil;
 	
 	@GetMapping("/notes")
-	public List<Note> getNotes() {
-		return noteService.findAll();	
+	public MappingJacksonValue getNotes() {
+		
+		List<Note> notes = noteService.findAll();	    
+	    
+		return filterUtil.dynamicSerializeAllExcept(notes, Note.class, Set.of("password","active"));	
+	}
+	
+	@GetMapping("/notes/{id}")
+	public MappingJacksonValue getNoteUsingPassword(@PathVariable String id, @RequestBody Note note) throws Exception {
+		Note decryptedNote =  noteService.getNoteByIdAndPassword(id,note.getPassword());	
+		return filterUtil.dynamicFilterOutAllExcept(decryptedNote, Note.class, Set.of("name","message"));
 	}
 	
 	@PostMapping("/notes")
-	public ResponseEntity<Object> createUser(@Valid @RequestBody Note note) {
+	public ResponseEntity<Object> createUser(@Valid @RequestBody Note note) throws Exception {
 		Note savedNote = noteService.save(note);
 
 		// return status of created user
